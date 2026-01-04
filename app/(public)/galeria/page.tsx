dc582e085422009f66c6955235ac62e86d478c9a
Christian Ivan Ledesma <cristuca1234@gmail.com>
2026-01-04 18:12:44 -0300

    feat: Skeletor como ui en las secciones de avisos y galeria + modificamos codigo sin afectar al funcionamiento del navbar pantalla grande ya que las secciones las aplicamos con map ya que usamos Link

--- app/(public)/galeria/page.tsx
+++ app/(public)/galeria/page.tsx
@@ -2,6 +2,7 @@
 import { useState,useEffect } from "react";
 import CardImgPublicadas from "@/app/components/public/cardImgPublicadas";
 import { toast } from "react-toastify";
+import SkeletonGaleria from "@/app/components/public/ui/SkeletonGaleria";
 
 
 export interface PropsImg{
@@ -14,23 +15,31 @@ createdAt:string;
 
 export default function Galeria() {
     const [imaPublicadas, setImaPublicadas] = useState<PropsImg[]>([]);
+    const [loading, setLoading] = useState(true);
 
     const obtenerImagenes=async()=>{
         try{
             const res = await fetch("/api/obtenerImagenesPublicadas");
             const data=await res.json();
-            console.log("DATA IMAGENES 👉", data);
             setImaPublicadas(data);
         }catch(error:unknown){
             console.log("Error al obtener las Imagenes Publicadas")
             toast.error("Error al descargar las imagenes")
 
+        } finally {
+            setLoading(false);
         }
     }
 
     useEffect(()=>{
         obtenerImagenes();
     },[])
+
+    if (loading) {
+        return (
+            <SkeletonGaleria />
+        );
+    }
     return (
        
         <CardImgPublicadas imaPublicadas={imaPublicadas}/>
